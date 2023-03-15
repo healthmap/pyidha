0d63f8ef3582d02f11e8f5f9718b392200496916
Chris Remmel <christopher.remmel@childrens.harvard.edu>
2023-03-15 15:08:24 -0400

    update models to be idiomatic sqlalchemy (?)

--- pyidha/models.py
+++ pyidha/models.py
@@ -1,27 +1,33 @@
 from datetime import datetime
-from sqlalchemy.orm import Session
 
-
-def write_repo_data(list_of_model_objects):
+def write_repo_data(list_of_model_objects, session):
     """Given a list of model objects, write them to the database.
+
+    This function is to be used inside an active Session context.
 
     Parameters
     __________
-    list_of_model_objects : list of obj
-        A list of model objects.
+    list_of_model_objects : list of Model
+        A list of SQLAlchemy Model objects.
+    session : Session
+        a SQLAlachemy Session object
 
     Returns
     _______
     None
 
-    """
-    with Session() as session:
-        try:
-            for item in list_of_model_objects:
-                item.last_update = datetime.now()  # last time the repo was updated
-                session.add(item)
-                session.commit()
-                print(f"successfully committed {item}")
+    Examples
+    ________
+    >>> with Session() as session:
+    ...     write_repo_data(list_of_model_objects, session)
 
-        except Exception as e:
-            print("write_repo_data", e)
+    """
+    try:
+        for item in list_of_model_objects:
+            item.last_update = datetime.now()  # last time the repo was updated
+            session.add(item)
+            session.commit()
+            print(f"successfully committed {item}")
+
+    except Exception as e:
+        print("write_repo_data", e)
